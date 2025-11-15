d1d459ccf177101f3ac8557ab7032fd1bc50dcee
Pedro Amaral <amaral.ph.m@gmail.com>
2025-11-15 11:33:49 -0300

    Configuring git pages

--- astro.config.ts
+++ astro.config.ts
@@ -22,6 +22,9 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
+  site: 'https://phamaralbr.github.io',
+  // base: '/dra-isadora-rosa',
+
   output: 'static',
 
   integrations: [
