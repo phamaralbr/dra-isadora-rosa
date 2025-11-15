fbf5050910d76737d1199d5e2a4ca45fb375a6b6
Pedro Amaral <amaral.ph.m@gmail.com>
2025-11-15 08:22:45 -0300

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import plugin from 'tailwindcss/plugin';
+import typographyPlugin from '@tailwindcss/typography';
+
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--aw-color-primary)',
+        secondary: 'var(--aw-color-secondary)',
+        accent: 'var(--aw-color-accent)',
+        default: 'var(--aw-color-text-default)',
+        muted: 'var(--aw-color-text-muted)',
+        isaGreen: {
+          DEFAULT: '#6f7257',
+          light: '#9ca080',
+          dark: '#4b4d3f',
+        },
+        isaPink: {
+          DEFAULT: '#8E6161',
+          light: '#b38c8c',
+          dark: '#664b4b',
+        },
+        offWhite: '#fffae9',
+      },
+      fontFamily: {
+        sans: ['var(--aw-font-sans, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
+        serif: ['var(--aw-font-serif, ui-serif)', ...defaultTheme.fontFamily.serif],
+        heading: ['var(--aw-font-heading, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
+      },
+
+      animation: {
+        fade: 'fadeInUp 1s both',
+      },
+
+      keyframes: {
+        fadeInUp: {
+          '0%': { opacity: 0, transform: 'translateY(2rem)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+    },
+  },
+  plugins: [
+    typographyPlugin,
+    plugin(({ addVariant }) => {
+      addVariant('intersect', '&:not([no-intersect])');
+    }),
+  ],
+  darkMode: 'class',
+};
