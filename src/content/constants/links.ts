91bbf60290efb39a997d5f0934b8d5948942b73f
Pedro Amaral <amaral.ph.m@gmail.com>
2025-11-20 12:00:11 -0300

    modularizing links

---
+++ src/content/constants/links.ts
@@ -0,0 +1,4 @@
+export const WHATSAPP_LINK = 'https://wa.me/5562985083888';
+export const INSTAGRAM_LINK = 'https://www.instagram.com/isadorarosa/';
+export const LATTES_LINK = '#';
+export const EMAIL_LINK = 'contato@draisadorarosa.com.br';
