9eee18746b7aa2c808a4c1aed2257f52ef3ce0ce
Pedro Amaral <amaral.ph.m@gmail.com>
2025-11-20 22:17:41 -0300

    Added lattes link

--- src/content/constants/links.ts
+++ src/content/constants/links.ts
@@ -1,4 +1,4 @@
 export const WHATSAPP_LINK = 'https://wa.me/5562985083888';
 export const INSTAGRAM_LINK = 'https://www.instagram.com/isadorarosa/';
-export const LATTES_LINK = '#';
+export const LATTES_LINK = 'http://lattes.cnpq.br/4933594574102569';
 export const EMAIL_LINK = 'contato@draisadorarosa.com.br';
