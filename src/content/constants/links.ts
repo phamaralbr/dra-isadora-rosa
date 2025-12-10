583214461551cd06ffc9a313dea50a1b23fcf97c
Pedro Amaral <amaral.ph.m@gmail.com>
2025-12-10 15:30:58 -0300

    Atualizando links e email de contato

--- src/content/constants/links.ts
+++ src/content/constants/links.ts
@@ -1,4 +1,5 @@
-export const WHATSAPP_LINK = 'https://wa.me/5562985083888';
+export const WHATSAPP_LINK =
+  'https://wa.me/5561993969142?text=Ol%C3%A1%2C%20gostaria%20de%20marcar%20uma%20consulta%20com%20a%20Dra.%20Isadora%20Rosa';
 export const INSTAGRAM_LINK = 'https://www.instagram.com/isadorarosa/';
 export const LATTES_LINK = 'http://lattes.cnpq.br/4933594574102569';
-export const EMAIL_LINK = 'contato@draisadorarosa.com.br';
+export const EMAIL_LINK = 'draisadorarosa@gmail.com';
