f5c11653d1e2cdd125618fa3ea5d06c910ca1352
Pedro Amaral <amaral.ph.m@gmail.com>
2026-03-03 15:40:11 -0300

    Changing contact link to linktree

--- src/content/constants/links.ts
+++ src/content/constants/links.ts
@@ -1,5 +1,4 @@
-export const WHATSAPP_LINK =
-  'https://wa.me/5561993969142?text=Ol%C3%A1%2C%20gostaria%20de%20marcar%20uma%20consulta%20com%20a%20Dra.%20Isadora%20Rosa';
+export const WHATSAPP_LINK = 'https://linktr.ee/draisadorarosa';
 export const INSTAGRAM_LINK = 'https://www.instagram.com/isadorarosa/';
 export const LATTES_LINK = 'http://lattes.cnpq.br/4933594574102569';
 export const EMAIL_LINK = 'draisadorarosa@gmail.com';
